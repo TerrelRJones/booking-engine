5b4a470feda8eba3c12693f9359786d96e9549ea
Jeric Hernandez <jeric.hernandez@bounteous.com>
2022-09-01 23:49:13 +0900

    chore: remove styledcomponent test code

--- src/components/TestComponent.styled.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components/native';
-
-export const StyledText = styled.Text`
-  background-color: blue;
-`;
--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { StyledText } from './TestComponent.styled';
 
 export const TestComponent = () => {
   //state
   //hooks
   return (
     <>
-      <StyledText>
-        <Text>TEST COMPONENT</Text>
-      </StyledText>
+      <Text>TEST COMPONENT</Text>
     </>
   );
 };
