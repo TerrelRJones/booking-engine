f226c5368924c1ad392241d25acf6ec92df85a6f
Jeric Hernandez <jeric.hernandez@bounteous.com>
2022-09-08 04:39:40 +0900

    feat: add CalendarList with hardcoded props for testing, needs state implementation

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,9 +1,29 @@
-import { Agenda, Calendar, CalendarList } from 'react-native-calendars';
+import React from 'react';
+import { Box } from 'native-base';
+import { CalendarList, DateData } from 'react-native-calendars';
 
 export const DatePicker = () => {
+  //get the initial date when pressed
+  //get the final date when pressed again
+  //get dates inbetween range
+  //change styles for every date inbetween
   return (
-    <>
-      <CalendarList />
-    </>
+    <React.Fragment>
+      <CalendarList
+        pastScrollRange={0}
+        futureScrollRange={1}
+        markingType={'period'}
+        markedDates={{
+          '2022-09-22': { startingDay: true, color: 'green' },
+          '2022-09-23': { color: 'green' },
+          '2022-09-30': {
+            selected: true,
+            endingDay: true,
+            color: 'green',
+          },
+        }}
+        onDayPress={(date: DateData) => console.log(date)}
+      />
+    </React.Fragment>
   );
 };
