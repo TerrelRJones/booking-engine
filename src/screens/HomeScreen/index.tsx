7bd490dd19605a2c30882212427d35f95acfc345
Terrel Jones <terrel.jones@bounteous.com>
2022-09-07 15:12:00 -0700

    feat: refactored HomeScreen and styled

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { Carousel } from 'components/Carousel/Carousel';
+import HomeScreenTitle from 'components/HomeScreenTitle';
+import SearchInput from 'components/SearchInput';
 import { Section } from 'components/Section/Section';
+import { UserPhoto } from 'components/UserPhoto/UserPhoto';
+import { COLORS } from 'const/colors';
 import { hotels as mockHotels } from 'mocks/hotelMocks';
 import { Box } from 'native-base';
+
+const image =
+  'https://media.architecturaldigest.com/photos/603fd63ce5e723e4f559fb16/16:9/w_2560%2Cc_limit/1300769954';
 
 const HomeScreen = () => {
   //make call with RTKQ here using hook thing
 
   return (
-    <Box flex={1}>
-      <Section header="TestHeader">
-        <Carousel hotels={mockHotels.hotels} />
-      </Section>
+    <Box flex={1} backgroundColor={COLORS.bgColor}>
+      <Box px={3} mt={5}>
+        {/* user photo  */}
+        <Box flexDirection="row" justifyContent="flex-end">
+          <UserPhoto img={image} />
+        </Box>
+
+        <HomeScreenTitle name="LeBron" />
+
+        <SearchInput placeHolder="Search Hotels..." />
+      </Box>
+
+      <Box pl={3}>
+        <Section header="Most Popular">
+          <Carousel hotels={mockHotels.hotels} />
+        </Section>
+      </Box>
     </Box>
   );
 };
