ff46d67ffe2f27f80b08f2c337c42ec10365c3de
Terrel Jones <terrel.jones@bounteous.com>
2022-09-07 15:36:13 -0700

    feat: added keyBoardType to SearchInputProps

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { COLORS } from 'const/colors';
 import { Box, Input, SearchIcon } from 'native-base';
+import { KeyboardTypeIOS } from 'react-native';
 
 interface SearchInputProps {
   placeHolder: string;
+  keyBoardType?: KeyboardTypeIOS;
 }
 
-export const SearchInput = ({ placeHolder }: SearchInputProps) => {
+export const SearchInput = ({
+  placeHolder,
+  keyBoardType,
+}: SearchInputProps) => {
   return (
     <Box mt={3}>
+      {/* TODO: show keyboard pop-up on ios  */}
       <Input
         placeholder={placeHolder}
         color={COLORS.textColor}
@@ -17,6 +23,7 @@ export const SearchInput = ({ placeHolder }: SearchInputProps) => {
         fontWeight="bold"
         InputRightElement={<SearchIcon mr={3} size={5} />}
         clearButtonMode="always"
+        keyboardType={keyBoardType}
         keyboardAppearance="dark"
       />
     </Box>
