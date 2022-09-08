b7de5a19815b6558ca6dec0a33edb8df9f9c53ad
Jeric Hernandez <jeric.hernandez@bounteous.com>
2022-09-09 03:45:34 +0900

    refactor: replace navigation bar with TopNavigation, resolve conflicts

--- src/components/CheckDate/CheckDate.tsx
+++ src/components/CheckDate/CheckDate.tsx
@@ -16,5 +16,3 @@ export const CheckDate = ({ checkType, date }: CheckDateProps) => {
     </Box>
   );
 };
-
-
--- src/screens/DatesScreen/index.tsx
+++ src/screens/DatesScreen/index.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
 import Button from 'components/Button/index';
 import DatePicker from 'components/DatePicker';
+import TopNavigation from 'components/TopNavigation';
 import { COLORS } from 'const/colors';
-import { Box, Text } from 'native-base';
+import { Box } from 'native-base';
 
 const DatesScreen = () => {
   return (
-    <Box flex={1} safeArea backgroundColor={COLORS.bgColor}>
-      <Box
-        paddingY={2}
-        backgroundColor={COLORS.secondaryBgColor}
-        justifyContent="space-between"
-        flexDirection="row">
-        <Text
-          color={COLORS.textColor}
-          paddingX={3}
-          fontWeight="bold"
-          onPress={() => goBack()}>
-          Go Back
-        </Text>
-      </Box>
+    <Box backgroundColor={COLORS.bgColor}>
+      <TopNavigation screenTitle="DatesScreen" />
       <DatePicker />
     </Box>
   );
