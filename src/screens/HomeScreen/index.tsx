bd34fbf65b809e0b44b567766194ccabbae97aba
Jeric Hernandez <jeric.hernandez@bounteous.com>
2022-09-10 07:44:11 +0900

    feat: add randomization for hotels so sections don't show same data, simulates tagged headers

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Carousel } from 'components/Carousel/Carousel';
 import HomeScreenTitle from 'components/HomeScreenTitle';
 import SearchInput from 'components/SearchInput';
 import { Section } from 'components/Section/Section';
 import { UserPhoto } from 'components/UserPhoto/UserPhoto';
 import { COLORS } from 'const/colors';
-import { hotels as mockHotels } from 'mocks/hotelMocks';
 import { Box, ScrollView } from 'native-base';
 import { useGetHotelsQuery } from 'services/hotelService';
+import { Hotel, Hotels } from 'types/hotelModels';
 
 const image =
   'https://media.architecturaldigest.com/photos/603fd63ce5e723e4f559fb16/16:9/w_2560%2Cc_limit/1300769954';
@@ -15,6 +15,13 @@ const image =
 const HomeScreen = () => {
   const { data: hotels } = useGetHotelsQuery(null);
   //pull user info from db? maybe add to mock json-server or maybe get fancy
+
+  const randomizeHotels = useCallback(
+    (hotels: Hotel[]) => {
+      return [...hotels].sort(() => 0.5 - Math.random());
+    },
+    [hotels],
+  );
 
   return (
     <Box flex={1} backgroundColor={COLORS.bgColor}>
@@ -31,13 +38,14 @@ const HomeScreen = () => {
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <Section header="Most Popular">
-          <Carousel hotels={hotels} />
+          {hotels && <Carousel hotels={randomizeHotels(hotels)} />}
         </Section>
+
         <Section header="Near You">
-          <Carousel hotels={hotels} />
+          {hotels && <Carousel hotels={randomizeHotels(hotels)} />}
         </Section>
         <Section header="All">
-          <Carousel hotels={hotels} />
+          {hotels && <Carousel hotels={randomizeHotels(hotels)} />}
         </Section>
       </ScrollView>
     </Box>
